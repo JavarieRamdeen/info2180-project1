1a85542a81c0a919f24d8edaa3f777f460a326b0
Javarie Ramdeen <JavarieRamdeen@gmail.com>
2021-10-24 20:13:48 -0500

    Edited Files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-/* Add your JavaScript to this file */
+"use strict";
+window.onload = function()
+{
+	let form = document.getElementById("email").value;
+	let state=  document.getElementsByClassName("message").innerHTML
+	let text; Text;
+	
+	if ((form.includes("@")) && (form.includes(".com"))){
+		text = "Thank you! Your email address has been added to our mailing list";
+	}else{
+		text = "Please enter a valid email address";
+	}
+	state = text;
+}
